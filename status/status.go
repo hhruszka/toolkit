dbfda919f5dd3eff05ddb8a99e1e5ef3683015bd
Henryk Hruszka <hhruszka@gmail.com>
2026-08-18 14:41:05 +0200

    Add `String()` method to `Status` for readable output.

--- status/status.go
+++ status/status.go
@@ -9,3 +9,19 @@ const (
 	Skipped // premise doesn't hold (notApplicable)
 	Error   // couldn't determine (open)
 )
+
+func (s Status) String() string {
+	switch s {
+	case Passed:
+		return "passed"
+	case Failed:
+		return "failed"
+	case Review:
+		return "review"
+	case Skipped:
+		return "skipped"
+	case Error:
+		return "error"
+	}
+	return "unknown"
+}
